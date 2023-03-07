e120b89bd84718ce24bc847faa432733bc25eb6a
Leonardo De Paula <92495561+leonarddepaula@users.noreply.github.com>
2023-03-07 11:21:51 -0300

    subindo vervidor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {InputTextModule} from 'primeng/inputtext';
-
+import{ HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {CardModule} from 'primeng/card';
 import { LoginComponent } from './views/login/login.component';
 import {ButtonModule} from 'primeng/button';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,8 @@ import {ButtonModule} from 'primeng/button';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule,
     CardModule,
     InputTextModule,
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { LoginService } from './../../resources/services/login.service';
+import { Component, OnInit } from '@angular/core';
+import { RequestLogin } from 'src/app/resources/models/RequestLogin';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public requestLogin!: RequestLogin;
 
+  constructor(private loginService: LoginService) {}
+
+  ngOnInit(): void {
+    this.requestLogin = new RequestLogin();
+  }
+
+  public doLogin() {
+    this.loginService.doLogin(this.requestLogin).subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
